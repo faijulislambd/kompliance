05467cf8b2bf0ec9f4618fdd4706cb04f7298a29
rejoanulferdoush <rejoanulferdoush@gmail.com>
2024-08-21 10:30:12 +0600

    Header BG Blur

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import Nav from "./Nav";
 const Header = () => {
   return (
     <header className="fixed w-full z-[50]">
-      <div className="header-menu-padding flex justify-between items-center absolute top-0 left-0 w-full ">
+      <div className=" header-blur-bg header-menu-padding flex justify-between items-center absolute top-0 left-0 w-full ">
         <Logo />
         <Nav></Nav>
       </div>
