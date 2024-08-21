254660e06bd4b626c27d8594e74395b705a26c16
rejoanulferdoush <rejoanulferdoush@gmail.com>
2024-08-21 10:19:32 +0600

    Text Added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,8 @@ const Hero = () => {
         src={gradientBG}
       />
       <div className="flex">
-        <div className="w-1/2">
+        <div className="w-2/3">
+          {/* Head 1 */}
           <h1 className="text-8xl text-white font-bold">
             Dare.
             <br />
@@ -18,6 +19,13 @@ const Hero = () => {
             Evolve.
             <br />
           </h1>
+          {/* Sub Text */}
+          <p className="my-4 text-4xl text-white">
+            We are Komplice, your Payroll and HRIS Integrated Service Center. We
+            dare to offer a personalized approach to the application of Payroll
+            and HRIS solutions, thus becoming partner of our clients'
+            performance and evolution.
+          </p>
         </div>
       </div>
     </section>
